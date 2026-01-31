32f07f48ae86341a2372becd880f7dd56eb63a5e
vmadmin <harrymoore67212@proton.me>
2026-01-31 10:57:25 +0000

    Add InputPlumber and OpenGamepadUI

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -2,6 +2,7 @@
 
 set -xeuo pipefail
 
+###Kernel
 for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
   rpm --erase $pkg --nodeps
 done
@@ -23,3 +24,18 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-a
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
   scx-scheds-git \
   scx-manager
+
+###InputPlumber
+dnf -y copr enable shadowblip/InputPlumber
+dnf -y copr disable shadowblip/InputPlumber
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:shadowblip:InputPlumber install \
+  inputplumber
+
+sudo systemctl enable inputplumber
+
+###OpenGamepadUI
+
+dnf install -y \
+    https://github.com/ShadowBlip/OpenGamepadUI/releases/download/v$OGUI_TAG/opengamepadui-$OGUI_TAG-1.x86_64.rpm \
+    https://github.com/ShadowBlip/InputPlumber/releases/download/v$IP_TAG/inputplumber-$IP_TAG-1.x86_64.rpm \
+    https://github.com/ShadowBlip/PowerStation/releases/download/v$PS_TAG/powerstation-$PS_TAG-1.x86_64.rpm
