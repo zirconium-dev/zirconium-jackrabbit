eb97f8669d13624d4285939c6f2651828468f6f4
Tulip Blossom <tulilirockz@outlook.com>
2026-02-01 11:55:46 -0300

    fix: only install inputplumber from COPR
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/00-gamerslop-fetch.sh
+++ build_files/00-gamerslop-fetch.sh
@@ -27,12 +27,10 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:shadowblip:InputPlumber insta
   inputplumber
 
 OGUI_TAG="$(curl --fail --retry 5 --retry-delay 5 --retry-all-errors -s "https://api.github.com/repos/ShadowBlip/OpenGamepadUI/releases/latest" | grep tag_name | cut -d : -f2 | tr -d 'v", ' | head -1)"
-IP_TAG="$(curl --fail --retry 5 --retry-delay 5 --retry-all-errors -s "https://api.github.com/repos/ShadowBlip/InputPlumber/releases/latest" | grep tag_name | cut -d : -f2 | tr -d 'v", ' | head -1)"
 PS_TAG="$(curl --fail --retry 5 --retry-delay 5 --retry-all-errors -s "https://api.github.com/repos/ShadowBlip/PowerStation/releases/latest" | grep tag_name | cut -d : -f2 | tr -d 'v", ' | head -1)"
 
 dnf install -y \
   "https://github.com/ShadowBlip/OpenGamepadUI/releases/download/v$OGUI_TAG/opengamepadui-$OGUI_TAG-1.$(arch).rpm" \
-  "https://github.com/ShadowBlip/InputPlumber/releases/download/v$IP_TAG/inputplumber-$IP_TAG-1.$(arch).rpm" \
   "https://github.com/ShadowBlip/PowerStation/releases/download/v$PS_TAG/powerstation-$PS_TAG-1.$(arch).rpm"
 
 dnf copr enable -y lizardbyte/beta
