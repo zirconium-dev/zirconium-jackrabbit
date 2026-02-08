4e79e854d3ab0d2024e2ee361666f8a92c9d425a
Tulip Blossom <tulilirockz@outlook.com>
2026-02-08 11:28:12 -0300

    chore(xone): remove wired kernel module and blacklist from it
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/00-gamerslop-post.sh
+++ build_files/00-gamerslop-post.sh
@@ -9,3 +9,6 @@ KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/
 dkms install -m "xone/$(basename "$(find /usr/src -iname "*xone*" -type d)" | cut -f2 -d-)" -k "${KERNEL_VERSION}"
 
 stat "/usr/lib/modules/${KERNEL_VERSION}"/extra/xone*.ko* # We actually need the kernel objects after build LOL
+rm "/usr/lib/modules/${KERNEL_VERSION}"/extra/xone_wired.ko* # XONE wired removes support for a _lot_ for hardware supported by xpad, we want that one instead here.
+sed -i "/xpad/d" /usr/lib/modprobe.d/*-xone.conf # This blacklists xpad by default
+grep -v "blacklist xpad" /usr/lib/modprobe.d/*-xone.conf
